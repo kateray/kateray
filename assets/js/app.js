a24c032b629f463daca6814c82a45d0f5e8dcdfc
Kate Ray <kateray@users.noreply.github.com>
2017-08-01 15:47:09 -0400

    mostly

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,5 @@
+var viewStyle = 'network'
+
 var App = {};
 App.category = null;
 
@@ -30,104 +32,281 @@ function parseData(data){
   return data;
 }
 
-$(document).ready(function() {
-  var width = $(window).width()-2;
-  var height = $(window).height()-2;
+function makeSimulation(height, width, data){
   var cx = width/2;
   var cy = height/2;
 
+
+  // Define all the forces
+  var linkForce = d3.forceLink(data.links)
+    .id(function(d) { return d.name; })
+    .distance(10)
+    .strength(0.2);
+
+  // keeps nodes from sliding off page
+  function boxForce(){
+    _.each(data.nodes, function(n) {
+      var distX = n.width/2;
+      var distY = n.height;
+
+      if (n.y < 250 || n.y > (height-50)) {
+        // if point is near top or bottom, constrain x in smaller box
+        n.x = Math.max(distX+100, Math.min(width - distX-250, n.x));
+      } else {
+        // otherwise just keep x 10px from left and right
+        n.x = Math.max(distX+10, Math.min(width - distX-10, n.x));
+      }
+      // keep y 15px from top and bottom
+      n.y = Math.max(distY+15, Math.min(height - distY-15, n.y));
+    });
+  }
+
+  // keeps nodes from overlapping, by moving their y value
+  function rectCollide() {
+    var strength = 0.25;
+    var padding = 2;
+    var t = data.nodes.length;
+    _.times(3, function(){
+      for (var i = 0; i < t; ++i) {
+        var a = data.nodes[i];
+        for (var j = i + 1; j < t; ++j) {
+          var b = data.nodes[j],
+            x = a.x + a.vx - b.x - b.vx,
+            y = a.y + a.vy - b.y - b.vy,
+            lx = Math.abs(x),
+            ly = Math.abs(y),
+            // both centered so add widths but divide by 2
+            w = (a.width + b.width + padding)/2,
+            // if a is above b, then use a's height, otherwise b's
+            h = (a.y > b.y) ? (a.height + padding) : (b.height + padding);
+          if (lx < w && ly < h) {
+            ly = (ly - h) * (y < 0 ? -strength : strength);
+            a.vy -= ly, b.vy += ly;
+          }
+        }
+      }
+    })
+  }
+
+  // puts project nodes in a circle with radius 200-300 around center
+  function concentricCircles() {
+    var smallRadius = 200;
+    var bigRadius = 300;
+    _.each(data.nodes, function(n) {
+      var strength = 3;
+      var distX = n.x - cx;
+      var distY = n.y - cy;
+      var dist = Math.sqrt(Math.pow(distX,2)+ Math.pow(distY,2));
+      // a^2 + b^2
+      var sumSquares = Math.pow(distX,2) + Math.pow(distY,2);
+      if (n.type === "project") {
+        // pythag theorem: if a^2 + b^2 <= csmall^2,
+        // then hypotenuse is smaller than radius of small circle
+        // point is inside small circle
+        if ( sumSquares <= Math.pow(smallRadius,2) ){
+          n.x = cx + distX / dist * smallRadius;
+          n.y = cy + distY / dist * smallRadius;
+        // if a^2 + b^2 > cbig^2
+        // then hypotenuse is bigger than radius of big circle
+        // point is outside big circle
+        } else if ( sumSquares > Math.pow(bigRadius,2) ){
+          n.x = cx + distX / dist * bigRadius;
+          n.y = cy + distY / dist * bigRadius;
+        }
+      }
+    });
+  }
+
+  var dragstarted = function(d) {
+    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+    d.fx = d.x;
+    d.fy = d.y;
+  }
+
+  var dragged = function(d) {
+    d.fx = d3.event.x;
+    d.fy = d3.event.y;
+  }
+
+  var dragended = function(d) {
+    if (!d3.event.active) simulation.alphaTarget(0);
+    d.fx = null;
+    d.fy = null;
+  }
+
+  var setNodeDimensions = function(d, i) {
+    data.nodes[i].width = this.getBoundingClientRect().width;
+    data.nodes[i].height = this.getBoundingClientRect().height;
+    if (data.nodes[i].id === "center") {
+      // fix center node
+      d.fx = cx;
+      d.fy = cy;
+    } else {
+      // start other nodes in random position
+      d.x = Math.random()*width;
+      d.y = Math.random()*height;
+    }
+  }
+
+  // var handleMouseover = function(d) {
+  //   if (d.href) {
+  //     node.style("fill", function(n) {
+  //       if (d === n) {
+  //         return "#0000ff";
+  //       }
+  //     });
+  //     node.attr("class", function(n) {
+  //       if (d === n) {
+  //         return "node hover " + n.type;
+  //       } else {
+  //         return "node " + n.type;
+  //       }
+  //     });
+  //   }
+  //   if (d.explanation) {
+  //     $("#explanation").html(d.explanation).css("opacity", 0.5);
+  //   }
+  //   link.style("stroke-width", function(l) {
+  //     if (d === l.source || d === l.target) {
+  //       return 0.5;
+  //     } else {
+  //       return 0.05;
+  //     }
+  //   });
+  //   return node.style("opacity", function(n) {
+  //     if (d === n) {
+  //       return 1;
+  //     }
+  //     if (_.find(data.links, function(l) {
+  //       return (d === l.source && n === l.target) || (n === l.source && d === l.target);
+  //     })) {
+  //       return 1;
+  //     } else {
+  //       return 0.1;
+  //     }
+  //   });
+  // }
+  //
+  // var handleMouseout = function(d) {
+  //   link.style("stroke-width", 0.05);
+  //   updateNodeOpacity();
+  //   $("#explanation").css("opacity", 0);
+  //   node.style("fill", "#000000");
+  //   return node.attr("class", function(n) {
+  //     return "node " + n.type;
+  //   });
+  // }
+
   var svg = d3.select("body").append("svg").attr("width", width).attr("height", height);
 
-  d3.json("data.json", function(json) {
-    var data = parseData(json);
+  var simulation = d3.forceSimulation()
+    .nodes(data.nodes)
+    .force("charge", d3.forceManyBody().strength(-800))
+    .force("concentric", concentricCircles)
+    .force("links", linkForce)
+    // .force("collide", rectCollide)
+    // .force("box_force", boxForce);
 
-    var linkForce = d3.forceLink(data.links)
-      .id(function(d) { return d.name; })
-      .distance(10)
-      .strength(0.2);
+  var link = svg.selectAll(".link")
+    .data(data.links)
+    .enter()
+    .append("line")
+    .attr("class", "link")
+    .style("opacity", 1)
+    .style("stroke-width", 0.05)
 
-    // keeps nodes from sliding off page
-    function boxForce(){
-      _.each(data.nodes, function(n) {
-        var distX = n.width/2;
-        var distY = n.height;
+  var node = svg.selectAll(".node")
+    .data(data.nodes, function(d){return d.id})
 
-        if (n.y < 250 || n.y > (height-50)) {
-          // if point is near top or bottom, constrain x in smaller box
-          n.x = Math.max(distX+100, Math.min(width - distX-250, n.x));
-        } else {
-          // otherwise just keep x 10px from left and right
-          n.x = Math.max(distX+10, Math.min(width - distX-10, n.x));
-        }
-        // keep y 15px from top and bottom
-        n.y = Math.max(distY+15, Math.min(height - distY-15, n.y));
-      });
-    }
+  node.exit().remove()
 
-    // keeps nodes from overlapping, by moving their y value
-    function rectCollide() {
-      var strength = 0.25;
-      var padding = 2;
-      var t = data.nodes.length;
-      _.times(3, function(){
-        for (var i = 0; i < t; ++i) {
-          var a = data.nodes[i];
-          for (var j = i + 1; j < t; ++j) {
-            var b = data.nodes[j],
-              x = a.x + a.vx - b.x - b.vx,
-              y = a.y + a.vy - b.y - b.vy,
-              lx = Math.abs(x),
-              ly = Math.abs(y),
-              // both centered so add widths but divide by 2
-              w = (a.width + b.width + padding)/2,
-              // if a is above b, then use a's height, otherwise b's
-              h = (a.y > b.y) ? (a.height + padding) : (b.height + padding);
-            if (lx < w && ly < h) {
-              ly = (ly - h) * (y < 0 ? -strength : strength);
-              a.vy -= ly, b.vy += ly;
-            }
-          }
-        }
-      })
-    }
+  var nodeEnter = node.enter()
+    .append("svg:a").attr("target", "_blank").attr("xlink:href", function(d){
+      return d.href
+    })
+    .append("text")
+    .attr("class", function(d){return "node " + d.type})
+    .text( function(d){return d.text})
+    // .style("opacity", nodeOpacity)
+    // .style("text-anchor", "middle")
+    // .call(d3.drag()
+    //   .on("start", dragstarted)
+    //   .on("drag", dragged)
+    //   .on("end", dragended));
+  var tickActions = function() {
+    // moves nodes and links on every tick
+    nodeEnter
+      .attr("x", function(d) { return d.x; })
+      .attr("y", function(d) { return d.y; });
+    link
+      .attr("x1", function(d) { return d.source.x; })
+      .attr("y1", function(d) { return d.source.y; })
+      .attr("x2", function(d) { return d.target.x; })
+      .attr("y2", function(d) { return d.target.y; });
+  }
+  simulation.on("tick", tickActions);
 
-    // puts project nodes in a circle with radius 200-300 around center
-    function concentricCircles() {
-      var smallRadius = 200;
-      var bigRadius = 300;
-      _.each(data.nodes, function(n) {
-        var strength = 3;
-        var distX = n.x - cx;
-        var distY = n.y - cy;
-        var dist = Math.sqrt(Math.pow(distX,2)+ Math.pow(distY,2));
-        // a^2 + b^2
-        var sumSquares = Math.pow(distX,2) + Math.pow(distY,2);
-        if (n.type === "project") {
-          // pythag theorem: if a^2 + b^2 <= csmall^2,
-          // then hypotenuse is smaller than radius of small circle
-          // point is inside small circle
-          if ( sumSquares <= Math.pow(smallRadius,2) ){
-            n.x = cx + distX / dist * smallRadius;
-            n.y = cy + distY / dist * smallRadius;
-          // if a^2 + b^2 > cbig^2
-          // then hypotenuse is bigger than radius of big circle
-          // point is outside big circle
-          } else if ( sumSquares > Math.pow(bigRadius,2) ){
-            n.x = cx + distX / dist * bigRadius;
-            n.y = cy + distY / dist * bigRadius;
-          }
-        }
-      });
-    }
+  // var nodeEnter = node.enter()
 
-    var simulation = d3.forceSimulation().nodes(data.nodes)
-      .force("charge", d3.forceManyBody().strength(-800))
-      .force("concentric", concentricCircles)
-      .force("links", linkForce)
-      .force("collide", rectCollide)
-      .force("box_force", boxForce);
+  // nodeEnter.each(setNodeDimensions)
+  // node.on("mouseover", handleMouseover)
+  // node.on("mouseout", handleMouseout)
 
-    var link = svg.selectAll(".link")
+  // var updateNodeOpacity = function() {
+  //   return node.style("opacity", function(n) {
+  //     return nodeOpacity(n);
+  //   });
+  // };
+  //
+  // var categoryOff = function() {
+  //   App.category = null;
+  //   $(".category").css("color", "black");
+  //   return updateNodeOpacity();
+  // };
+  //
+  // var categoryOn = function() {
+  //   $(".category[data=" + App.category + "]").css("color", "purple");
+  //   return updateNodeOpacity();
+  // };
+  //
+  // $(".category").click(function() {
+  //   var category;
+  //   category = $(this).attr("data");
+  //   if (App.category === category) {
+  //     return categoryOff();
+  //   } else {
+  //     categoryOff();
+  //     App.category = category;
+  //     return categoryOn();
+  //   }
+  // });
+
+
+
+  var listView = function(){
+    simulation.on("tick", null)
+
+    var projects = data.nodes.filter(function(n){
+      return n.type === 'project'
+    })
+
+    console.log(nodeEnter)
+    nodeEnter
+      .data(projects, function(d) {return d.id})
+      .exit()
+      .remove()
+
+    nodeEnter
+      .transition()
+      .attr("x", 200)
+      // .style("text-anchor", "start")
+    console.log(nodeEnter)
+    link.remove()
+
+  }
+
+  var networkView = function(){
+    link
       .data(data.links)
       .enter()
       .append("line")
@@ -135,144 +314,61 @@ $(document).ready(function() {
       .style("opacity", 1)
       .style("stroke-width", 0.05)
 
-    var node = svg.selectAll(".node")
-      .data(data.nodes)
+    console.log(data.nodes)
+
+    nodeEnter.remove()
+    
+    node
+      .data(data.nodes, function(d){console.log(d.id); return d.id})
+      // .exit()
+      // .remove()
+
+    nodeEnter = node
       .enter()
+      // .merge(node)
+      // .merge(nodeEnter)
+      // .append("div")
+      // .attr("class", function(d){
+      //   return 'test ' + d.id
+      // })
       .append("svg:a").attr("target", "_blank").attr("xlink:href", function(d){
         return d.href
       })
       .append("text")
       .attr("class", function(d){return "node " + d.type})
       .text( function(d){return d.text})
-      .style("opacity", nodeOpacity)
-      .style("text-anchor", "middle")
-      .call(d3.drag()
-        .on("start", dragstarted)
-        .on("drag", dragged)
-        .on("end", dragended));
+      // .style("opacity", nodeOpacity)
+      // .style("text-anchor", "middle")
+      // .merge(node)
 
-    node.each(function(d, i){
-      data.nodes[i].width = this.getBoundingClientRect().width;
-      data.nodes[i].height = this.getBoundingClientRect().height;
-      if (data.nodes[i].id === "center") {
-        // fix center node
-        d.fx = cx;
-        d.fy = cy;
-      } else {
-        // start other nodes in random position
-        d.x = Math.random()*width;
-        d.y = Math.random()*height;
-      }
-    });
+    // node.each(setNodeDimensions)
+    // node.on("mouseover", handleMouseover)
+    // node.on("mouseout", handleMouseout)
 
-    function dragstarted(d) {
-      if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-      d.fx = d.x;
-      d.fy = d.y;
-    }
-
-    function dragged(d) {
-      d.fx = d3.event.x;
-      d.fy = d3.event.y;
-    }
-
-    function dragended(d) {
-      if (!d3.event.active) simulation.alphaTarget(0);
-      d.fx = null;
-      d.fy = null;
-    }
-
-    var tickActions = function() {
-      // moves nodes and links on every tick
-      node
-        .attr("x", function(d) { return d.x; })
-        .attr("y", function(d) { return d.y; });
-      link
-        .attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
-    }
-
+    // simulation.nodes(data.nodes)
     simulation.on("tick", tickActions);
+    simulation.alpha(1).restart()
+  }
 
-    node.on("mouseover", function(d) {
-      if (d.href) {
-        node.style("fill", function(n) {
-          if (d === n) {
-            return "#0000ff";
-          }
-        });
-        node.attr("class", function(n) {
-          if (d === n) {
-            return "node hover " + n.type;
-          } else {
-            return "node " + n.type;
-          }
-        });
-      }
-      if (d.explanation) {
-        $("#explanation").html(d.explanation).css("opacity", 0.5);
-      }
-      link.style("stroke-width", function(l) {
-        if (d === l.source || d === l.target) {
-          return 0.5;
-        } else {
-          return 0.05;
-        }
-      });
-      return node.style("opacity", function(n) {
-        if (d === n) {
-          return 1;
-        }
-        if (_.find(data.links, function(l) {
-          return (d === l.source && n === l.target) || (n === l.source && d === l.target);
-        })) {
-          return 1;
-        } else {
-          return 0.1;
-        }
-      });
-    });
+  $('.toggle-view').click(function(){
+    if (viewStyle === 'network') {
+      viewStyle = 'list'
+      $(this).text('graph view')
+      listView()
+    } else {
+      viewStyle = 'network'
+      $(this).text('chronological view')
+      networkView()
+    }
+  })
 
-    function updateNodeOpacity() {
-      return node.style("opacity", function(n) {
-        return nodeOpacity(n);
-      });
-    };
+}
 
-    node.on("mouseout", function(d) {
-      link.style("stroke-width", 0.05);
-      updateNodeOpacity();
-      $("#explanation").css("opacity", 0);
-      node.style("fill", "#000000");
-      return node.attr("class", function(n) {
-        return "node " + n.type;
-      });
-    });
+$(document).ready(function() {
+  var width = $(window).width()-2;
+  var height = $(window).height()-2;
 
-    var categoryOff = function() {
-      App.category = null;
-      $(".category").css("color", "black");
-      return updateNodeOpacity();
-    };
-
-    var categoryOn = function() {
-      $(".category[data=" + App.category + "]").css("color", "purple");
-      return updateNodeOpacity();
-    };
-
-    $(".category").click(function() {
-      var category;
-      category = $(this).attr("data");
-      if (App.category === category) {
-        return categoryOff();
-      } else {
-        categoryOff();
-        App.category = category;
-        return categoryOn();
-      }
-    });
-
+  d3.json("data.json", function(json) {
+    makeSimulation(height, width, parseData(json))
   })
 });
